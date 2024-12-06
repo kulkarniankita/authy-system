1d1f1de025a0a1dbcc292091575d88aee5a8ad0a
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2024-12-06 14:24:48 -0500

    Creating a Credentials Provider with Next Auth

--- auth.ts
+++ auth.ts
@@ -1,5 +1,17 @@
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        username: { label: 'Username' },
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize: async (credentials) => {
+        console.log({ credentials });
+        return null;
+      },
+    }),
+  ],
 });
